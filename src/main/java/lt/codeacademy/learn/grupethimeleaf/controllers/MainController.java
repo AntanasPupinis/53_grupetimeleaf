5b448aa7c77120f36345d6c60ccfe796a7950c88
Antanas <pupinis.a@gmail.com>
2023-03-23 14:27:29 +0200

    comitas pridetas controlleriai

---
+++ src/main/java/lt/codeacademy/learn/grupethimeleaf/controllers/MainController.java
@@ -0,0 +1,24 @@
+package lt.codeacademy.learn.grupethimeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/index1")
+	public String index1() {
+		return "index";
+	}
+	
+	@GetMapping("/index2")
+	public String index2() {
+		return "index2";
+}
+	@GetMapping("/index3")
+	public String index3() {
+		return "index3";
+	}
+	
+	
+}
